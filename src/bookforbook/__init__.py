b43ae85de87fd06821b86d79551d802a34e4e057
vikas-parashar <svnitvikas@gmail.com>
2015-08-19 00:17:37 +0530

    templates added

---
+++ src/bookforbook/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__about__ = """
+This project is similar to pinax-project-account in providing a foundation for
+sites that have user accounts but it differs in supporting social
+authentication rather than local password based authentication.
+"""
--- src/manage.py
+++ src/manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+
 import os
 import sys
 
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookforbook.settings")
-
     from django.core.management import execute_from_command_line
-
+    import bookforbook.startup as startup
+    startup.run()
     execute_from_command_line(sys.argv)
